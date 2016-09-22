b37c8448b9d70e3107daf6b6c2d38a0c9758c68e
Roberto Fonseca <roberto@rhfactor.com.br>
2016-09-21 21:53:52 -0300

    Creating Eclipse Web project

---
+++ src/test/java/br/com/rhfactor/entityscaffolding/entities/SimpleTypeEntity.java
@@ -0,0 +1,17 @@
+package br.com.rhfactor.entityscaffolding.entities;
+
+import java.math.BigDecimal;
+
+public class SimpleTypeEntity {
+	
+	private Long id;
+	private String name;
+	private Double value;
+	private Integer age;
+	private int quantity;
+	private double active;
+	private BigDecimal price;
+	private char initial;
+	 
+
+}
