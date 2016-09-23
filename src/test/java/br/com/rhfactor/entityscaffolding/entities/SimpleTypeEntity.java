741487301f6f665f203aa01244f2a1537f051d42
Roberto Fonseca <roberto@rhfactor.com.br>
2016-09-22 23:57:07 -0300

    Mapping Simple Values Entity

--- src/test/java/br/com/rhfactor/entityscaffolding/entities/SimpleTypeEntity.java
+++ src/test/java/br/com/rhfactor/entityscaffolding/entities/SimpleTypeEntity.java
@@ -2,16 +2,116 @@ package br.com.rhfactor.entityscaffolding.entities;
 
 import java.math.BigDecimal;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "simpleType")
 public class SimpleTypeEntity {
-	
+
+	@Id
+	@GeneratedValue
 	private Long id;
+
+	@Column(nullable = false, length = 50)
 	private String name;
+
+	@Column(nullable = true, length = 50)
+	private String title;
+
+	@Lob
+	@Column(nullable = true, length = 500)
+	private String description;
+
+	private char initial;
 	private Double value;
+	private double percentual;
 	private Integer age;
 	private int quantity;
-	private double active;
 	private BigDecimal price;
-	private char initial;
-	 
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public char getInitial() {
+		return initial;
+	}
+
+	public void setInitial(char initial) {
+		this.initial = initial;
+	}
+
+	public Double getValue() {
+		return value;
+	}
+
+	public void setValue(Double value) {
+		this.value = value;
+	}
+
+	public double getPercentual() {
+		return percentual;
+	}
+
+	public void setPercentual(double percentual) {
+		this.percentual = percentual;
+	}
+
+	public Integer getAge() {
+		return age;
+	}
+
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
 
 }
